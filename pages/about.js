5f7c773196705c7cfd958d7e32ef33251d0e117d
ksamy90 <ksamy90@aol.com>
2022-01-24 09:54:18 +0300

    add eventsfrontend repo

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ export default function AboutPage() {
     <Layout title="about page">
       <h1>About Page</h1>
       <p>This is a site for finding music events</p>
+      <p>jean</p>
     </Layout>
   );
 }
